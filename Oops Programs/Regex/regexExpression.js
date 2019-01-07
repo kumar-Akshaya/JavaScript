4aec89fb4f1814b58fbba77b37eba19d6ddada72
mounikanarla <nmounika358@gmail.com>
2019-01-07 13:25:11 +0530

    first day code

---
+++ Oops Programs/Regex/regexExpression.js
@@ -0,0 +1,31 @@
+var readline=require('readline-sync');
+var access=require('../Oops Programs/Utility');
+
+function regex()
+{
+    
+  var name=readline.question("Enter your name ");
+  while(isNaN(name)==false)
+  {
+      console.log("Please, Re-enter your name ");
+      var name=readline.question("Enter your name ");
+  }
+  var fullname=readline.question("Enter your Full name ");
+  while(isNaN(fullname)==false)
+  {
+      console.log("Please,Re-enter your fullname ");
+      var fullname=readline.question("Enter your fullname ");
+  }
+  var contact=readline.question("Enter your Mobile number");
+  while(isNaN(contact)==true || contact.length!=10)
+  {
+      console.log("Please, Re-enter your mobile number ");
+      var contact=readline.question("Enter your mobile number ");
+  }
+
+  var date=new Date();
+  var answer=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+  console.log(answer);
+  access.regexExpression(name,fullname,contact,answer);
+}
+regex();
